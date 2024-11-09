82d760dafb8a9363f97c2cd2529c4925ca49a775
Folau Kaveinga <folaukaveinga@gmail.com>
2024-11-09 16:40:02 -0700

    setup

---
+++ src/main/java/com/folau/file/SpringbootWithFileApplication.java
@@ -0,0 +1,13 @@
+package com.folau.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithFileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/folau/file/SpringbootWithFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.folau.file;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
